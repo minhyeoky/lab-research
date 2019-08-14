a7ae92eb56d520e4a25e318b92858a698b5ad1c7
Lee, Jaeyoung <wayexists02@gmail.com>
2019-08-14 11:05:52 +0900

    I made DataLoader to load sound data in batch length.

---
+++ preprocess.py
@@ -0,0 +1,35 @@
+import numpy as np
+import pandas as pd
+import pathlib
+import os
+
+from functools import partial
+from env import INFO_FILE_PATH
+
+
+def read_list():
+    file_list = []
+
+    info_file = pd.read_excel(INFO_FILE_PATH, sheet_name="Sheet1")
+
+    def append_to_file_list(row, file_list):
+        file_name = f"./data/{int(row['fileName'])}_{int(row['suffix'])}.wav"
+        text = row["text"]
+        sex = row["sex"]
+        langNat = row["langNat"]
+        levKor = row["levKor"]
+
+        file_list.append({
+                "fileName": file_name,
+                "text": text,
+                "sex": sex.replace("남", "M").replace("여", "F"),
+                "langNat": langNat,
+                "levKor": levKor
+        })
+
+        return None
+        
+    info_file.apply(partial(append_to_file_list, file_list=file_list), axis=1)
+
+    return file_list
+
