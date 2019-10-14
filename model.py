30832243d1c6d69c0c93f925202bd3b59ce2e16c
minhyeoklee <rhodochrosited@gmail.com>
2019-10-15 00:46:38 +0900

    #12 기초적인 DCGAN 구현

--- model.py
+++ model.py
@@ -7,6 +7,7 @@ BatchNorm = keras.layers.BatchNormalization
 Conv2D = keras.layers.Conv2D
 DConv = keras.layers.Conv2DTranspose
 Dense = keras.layers.Dense
+Dropout = keras.layers.Dropout
 
 
 def _get_embedding_table(checkpoint_file):
@@ -24,14 +25,18 @@ def _get_embedding_table(checkpoint_file):
     return embed_table
 
 
-class AutoEncoder(keras.models.Model):
+class Generator(keras.models.Model):
+    def compute_output_signature(self, input_signature):
+        pass
+
     def __init__(self, config, checkpoint_file, input_shape, **kwargs):
-        super(AutoEncoder, self).__init__(**kwargs)
+        super(Generator, self).__init__(**kwargs)
         self.config = load_config(config)
         self.audio_shape = input_shape
 
         self.act_fn = keras.layers.LeakyReLU()
         self.kernel_size = self.config['kernel_size']
+        self.max_len = self.config['max_len']
 
         self.embedding_table = _get_embedding_table(checkpoint_file)
 
@@ -51,10 +56,7 @@ class AutoEncoder(keras.models.Model):
             self.act_fn,
             Conv2D(filters=512, kernel_size=self.kernel_size, strides=(2, 2), padding='same', activation=None),
             BatchNorm(),
-            self.act_fn,
-            #             keras.layers.Flatten(),
-            #             Dense(units=output_size, activation=None),
-            #             self.act_fn
+            self.act_fn
         ]
 
         self.dconv = [
@@ -102,7 +104,81 @@ class AutoEncoder(keras.models.Model):
     def _concat_text(self, audio, text):
         text = tf.gather(self.embedding_table, text)
         text = self.embed_hidden(text)
-        text = tf.reshape(text, shape=[-1, 32, *self.hidden_size])
+        text = tf.reshape(text, shape=[-1, self.max_len, *self.hidden_size])
         text = tf.transpose(text, perm=[0, 2, 3, 1])
+        audio = tf.concat([audio, text], axis=-1)
+        return audio
+
+
+class Discriminator(keras.models.Model):
+    def compute_output_signature(self, input_signature):
+        pass
+
+    def __init__(self, config, input_shape, checkpoint_file, **kwargs):
+        super(Discriminator, self).__init__(**kwargs)
+        self.config = load_config(config)
+        self.audio_shape = input_shape
+
+        self.rate = self.config['dropout_rate']
+        self.alpha = 0.3
+        self.kernel_size = self.config['kernel_size']
+        self.max_len = self.config['max_len']
+        self.batch_size = self.config['batch_size']
+
+        self.act_fn = keras.layers.LeakyReLU(alpha=self.alpha)
+        self.embedding_table = _get_embedding_table(checkpoint_file)
+
+        self.conv = [
+            Conv2D(filters=32, kernel_size=self.kernel_size, padding='same', strides=(2, 2), activation=None),
+            Dropout(rate=self.rate),
+            self.act_fn,
+            Conv2D(filters=64, kernel_size=self.kernel_size, padding='same', strides=(2, 2), activation=None),
+            Dropout(rate=self.rate),
+            self.act_fn,
+            Conv2D(filters=128, kernel_size=self.kernel_size, padding='same', strides=(2, 2), activation=None),
+            Dropout(rate=self.rate),
+            self.act_fn,
+            Conv2D(filters=256, kernel_size=self.kernel_size, padding='same', strides=(2, 2), activation=None),
+            Dropout(rate=self.rate),
+            self.act_fn,
+            Conv2D(filters=512, kernel_size=self.kernel_size, padding='same', strides=(2, 2), activation=None),
+            Dropout(rate=self.rate),
+            self.act_fn,
+            keras.layers.Flatten()
+        ]
+        self.prob = [
+            Dense(units=512, activation=None),
+            Dropout(rate=self.rate),
+            self.act_fn,
+            Dense(units=1, activation=None),
+            keras.layers.Activation('sigmoid')
+        ]
+        self.embed_hidden = Dense(units=self.hidden_size[0] * self.hidden_size[1])
+
+    @tf.function
+    def call(self, inputs, **kwargs):
+        audio, text = inputs
+        audio = tf.expand_dims(audio, -1)
+
+        for layer in self.conv:
+            audio = layer(audio)
+
+        audio = self._concat_text(audio, text)
+
+        for layer in self.prob:
+            audio = layer(audio)
+
+        prob = audio
+        return prob
+
+    @property
+    def hidden_size(self):
+        return int(self.audio_shape[0] / 2 ** 5), int(self.audio_shape[1] / 2 ** 5)
+
+    def _concat_text(self, audio, text):
+        text = tf.gather(self.embedding_table, text)
+        text = self.embed_hidden(text)
+        text = tf.reshape(text, shape=[-1, self.max_len * self.hidden_size[0] * self.hidden_size[1]])
+        #         text = tf.transpose(text, perm=[0, 2, 3, 1])
         audio = tf.concat([audio, text], axis=-1)
         return audio
--- train.py
+++ train.py
@@ -8,7 +8,7 @@ import numpy as np
 from functools import partial
 from pathlib import Path
 import argparse
-from model import AutoEncoder
+from model import Generator, Discriminator
 import os
 from time import time
 
@@ -51,9 +51,13 @@ if not ckpt_dir.exists():
 
 # Data
 dl: DataLoader = DataLoader(config=args.config, data=args.data_dir, n_max=n_max)
-dataset_train = tf.data.Dataset.from_generator(partial(dl.train_generator, data='hub'),
+dataset_audio = tf.data.Dataset.from_generator(partial(dl.train_generator, data='hub'),
                                                output_types=tf.float32,
                                                output_shapes=dl.stft_shape)
+dataset_text = tf.data.Dataset.from_generator(partial(dl.train_generator, data='hub', return_text=True),
+                                              output_types=tf.int32,
+                                              output_shapes=dl.text_shape)
+dataset_train = tf.data.Dataset.zip((dataset_audio, dataset_text))
 dataset_train = dataset_train.shuffle(buffer_size=batch_size * 10).batch(batch_size)
 
 # Train setup
@@ -85,6 +89,20 @@ def loss_l1(x_train, pred):
     return loss
 
 
+def loss_d(real, fake):
+    loss_real = keras.losses.binary_crossentropy(tf.ones_like(real), real)
+    loss_fake = keras.losses.binary_crossentropy(tf.zeros_like(fake), fake)
+    loss = loss_real + loss_fake
+    loss = tf.reduce_mean(loss)
+    return loss
+
+
+def loss_g(fake):
+    loss = keras.losses.binary_crossentropy(tf.ones_like(fake), fake)
+    loss = tf.reduce_mean(loss)
+    return loss
+
+
 def _pred_to_img(x):
     fig = dl.specshow(x, return_figure=True, mel=False)
     fig.savefig('./temp.png')
@@ -101,37 +119,49 @@ def _pred_to_audio(x):
     return x
 
 
-def _summary_grads(grads_and_vars, step):
+def _summary_grads(grads_and_vars, step, model):
     # https://stackoverflow.com/questions/48709128/plot-gradients-of-individual-layers-in-tensorboard
     with writer_train.as_default():
         for grad, var in grads_and_vars:
             if grad is not None:
-                tf.summary.histogram(name=f'{var.name}/grads/histogram', data=grad, step=step)
-                tf.summary.scalar(name=f'{var.name}/grads/mean', data=tf.reduce_mean(grad), step=step)
+                tf.summary.histogram(name=f'{model}-{var.name}/grads/histogram', data=grad, step=step)
+                tf.summary.scalar(name=f'{model}-{var.name}/grads/mean', data=tf.reduce_mean(grad), step=step)
 
 
 @tf.function
 def train_step(x_train, step):
-    with tf.GradientTape(persistent=True) as tape:
-        pred = model(x_train)
-        loss = loss_l1(x_train, pred)
-    grads = tape.gradient(loss, model.trainable_variables)
-    grads_and_vars = zip(grads, model.trainable_variables)
-    _summary_grads(grads_and_vars, step)
-    optimizer.apply_gradients(zip(grads, model.trainable_variables))
+    with tf.GradientTape() as tape_g, tf.GradientTape() as tape_d:
+        pred = gen(x_train)
+        probs_fake = dis((pred, x_train[1]))
+        probs_real = dis(x_train)
 
-    return pred, loss
+        _loss_g = loss_l1(x_train[0], pred) + loss_g(probs_fake)
+        _loss_d = loss_d(probs_real, probs_fake)
+
+    grads_g = tape_g.gradient(_loss_g, gen.trainable_variables)
+    grads_and_vars = zip(grads_g, gen.trainable_variables)
+    _summary_grads(grads_and_vars, step, model='Generator')
+    opt_g.apply_gradients(zip(grads_g, gen.trainable_variables))
+
+    grads_d = tape_d.gradient(_loss_d, dis.trainable_variables)
+    grads_and_vars = zip(grads_d, dis.trainable_variables)
+    _summary_grads(grads_and_vars, step, model='Discriminator')
+    opt_d.apply_gradients(zip(grads_d, dis.trainable_variables))
+
+    return pred, (_loss_g, _loss_d)
 
 
 # Train
 # step = tf.constant(1, dtype=tf.int64)
 
-# Load model
-model = AutoEncoder(config=args.config, input_shape=dl.stft_shape, checkpoint_file=checkpoint_file)
-optimizer = keras.optimizers.Adam(learning_rate=learning_rate)
+# Load gen
+gen = Generator(config=args.config, input_shape=dl.stft_shape, checkpoint_file=checkpoint_file)
+dis = Discriminator(config=args.config, input_shape=dl.stft_shape, checkpoint_file=checkpoint_file)
+opt_g = keras.optimizers.Adam(learning_rate=learning_rate, beta_1=0.5)
+opt_d = keras.optimizers.Adam(learning_rate=learning_rate, beta_1=0.5)
 
 # https://www.tensorflow.org/guide/checkpoint
-ckpt = tf.train.Checkpoint(step=tf.Variable(1, dtype=tf.int64), optimizer=optimizer, net=model)
+ckpt = tf.train.Checkpoint(step=tf.Variable(1, dtype=tf.int64), optimizer=opt_g, net=gen)
 manager = tf.train.CheckpointManager(ckpt, ckpt_dir, max_to_keep=10)
 ckpt.restore(manager.latest_checkpoint)
 if manager.latest_checkpoint:
@@ -147,26 +177,30 @@ for epoch in range(epochs):
 
         if int(ckpt.step) % 50 == 0:
             with writer_train.as_default():
-                tf.summary.scalar(name='loss',
-                                  data=loss,
+                tf.summary.scalar(name='Loss-Generator',
+                                  data=loss[0],
                                   step=step,
-                                  description='loss 테스트')
-                tf.summary.image(name='spectrogram',
+                                  description='Total Loss of Generator')
+                tf.summary.scalar(name='Loss-Discriminator',
+                                  data=loss[1],
+                                  step=step,
+                                  description='Total Loss of Discriminator')
+                tf.summary.image(name='Spectrogram',
                                  data=tf.map_fn(_pred_to_img, pred),
                                  max_outputs=3,
                                  step=step,
-                                 description='image 테스트')
-                tf.summary.audio(name='audio test',
+                                 description='복원된 스펙트로그')
+                tf.summary.audio(name='Restored Audio',
                                  data=tf.expand_dims(tf.map_fn(_pred_to_audio, pred), axis=-1),
                                  sample_rate=dl.sr,
                                  step=step,
                                  max_outputs=3)
 
         if step % save_step == 0:
-            # tf.saved_model.save(model, ckpt_dir=ckpt_dir)
+            # tf.saved_model.save(gen, ckpt_dir=ckpt_dir)
             save_path = manager.save()
             print(f'Saved checkpoint for step {ckpt.step.numpy()}: {save_path}')
-            print(f'Loss {loss.numpy()}')
+            print(f'Loss {loss.numpy()[0] + loss.numpy()[1]}')
         ckpt.step.assign_add(1)
 
     end = time()
